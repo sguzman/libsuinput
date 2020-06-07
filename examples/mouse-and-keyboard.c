cef37ef12bdc6e5622dd173d922ac0169d0f1644
Salvador Guzman <guzmansalv@gmail.com>
2020-06-06 21:56:30 -0700

    added a nother mouse example for clicking

--- examples/mouse-and-keyboard.c
+++ examples/mouse-and-keyboard.c
@@ -6,7 +6,7 @@
 
 #include <suinput.h>
 
-int main(void) {
+void mouse(void) {
     int uinput_fd;
     int btns[] = {BTN_LEFT, BTN_RIGHT, BTN_MIDDLE};
     int rel_axes[] = {REL_X, REL_Y, REL_WHEEL};
@@ -44,8 +44,57 @@ int main(void) {
     }
 
     suinput_emit_click(uinput_fd, BTN_LEFT);
+    suinput_destroy(uinput_fd);
+}
+void keyboard(void)
+{
+    int i;
+    int uinput_fd;
+    int keys[] = {KEY_E, KEY_H, KEY_L, KEY_O};
+    struct uinput_user_dev user_dev;
+
+    memset(&user_dev, 0, sizeof(struct uinput_user_dev));
+    strcpy(user_dev.name, "libsuinput-example-keyboard");
+
+    uinput_fd = suinput_open();
+
+    if (uinput_fd == -1)
+        err(1, "suinput_open");
+
+    /* Error handling is omitted to keep code as readible as possible. */
+
+    for (i = 0; i < 4; ++i) {
+        suinput_enable_event(uinput_fd, EV_KEY, keys[i]);
+    }
+
+    suinput_create(uinput_fd, &user_dev);
+    sleep(1);
+
+    suinput_emit_click(uinput_fd, KEY_H);
+    suinput_syn(uinput_fd);
+    sleep(1);
+
+    suinput_emit_click(uinput_fd, KEY_E);
+    suinput_syn(uinput_fd);
+    sleep(1);
+
+    suinput_emit_click(uinput_fd, KEY_L);
+    suinput_syn(uinput_fd);
+    sleep(1);
+
+    suinput_emit_click(uinput_fd, KEY_L);
+    suinput_syn(uinput_fd);
+    sleep(1);
+
+    suinput_emit_click(uinput_fd, KEY_O);
+    suinput_syn(uinput_fd);
+    sleep(1);
 
     suinput_destroy(uinput_fd);
+}
 
+int main(void) {
+    mouse();
+    keyboard();
     return 0;
 }
